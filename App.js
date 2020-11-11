61bd70f83a6ec6373b64a500ec73746c1e1232cd
MaheshB77 <mbansode7696@gmail.com>
2020-11-11 10:26:12 +0530

    Changed the local repository

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Aishi</Text>
+            <Text style={styles.demoText}>Mahesh</Text>
+            <Text style={styles.demoText1}>Mahesh</Text>
+            <Button
+                // onPress={onPressLearnMore}
+                title="Press me"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            />
             <StatusBar style="auto" />
         </View>
     );
@@ -18,5 +25,19 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         backgroundColor: "green"
+    },
+    demoText: {
+        fontSize: 30,
+        backgroundColor: "white",
+        width: 200,
+        textAlign: "center"
+    },
+    demoText1: {
+        fontSize: 30,
+        backgroundColor: "yellow",
+        width: 200,
+        textAlign: "center",
+        borderRadius: 10,
+        marginTop: 5
     }
 });
